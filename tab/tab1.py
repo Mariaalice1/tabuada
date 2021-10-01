acfa5c8ce07048d77f37a168818e80c7d77c208b
Mariaalice1 <85592927+Mariaalice1@users.noreply.github.com>
2021-10-01 13:24:34 -0300

    tab.subtração

--- tab/tab1.py
+++ tab/tab1.py
@@ -130,3 +130,135 @@ for g in tab_9:
 for h in tab_10:
     print("10+4 = {}".format(h))
     print ("----------------------------------------------")
+
+#Subtracao
+tab = [2-1]
+tab_2 = [3-1]
+tab_3 = [4-1]
+tab_4 = [5-1]
+tab_5 = [6-1]
+tab_6 = [7-1]
+tab_7 = [8-1]
+tab_8 = [9-1]
+tab_9 = [10-1]
+
+for y in tabb:
+    print("1-1 = {}".format(y))
+for i in tab:
+    print("2-1 = {}".format(i))
+for a in tab_2:
+    print("3-1 = {}".format(a))
+for b in tab_3:
+    print("4-1 = {}".format(b))
+for c in tab_4:
+    print("5-1 = {}".format(c))  
+for d in tab_5:
+    print("6-1 = {}".format(d))
+for e in tab_6:
+    print("7-1 = {}".format(e))
+for f in tab_7:
+    print("8-1 = {}".format(f))
+for g in tab_8:
+    print("9-1 = {}".format(g))
+for h in tab_9:
+    print("10-1 = {}".format(h))
+    print ("----------------------------------------------")
+    
+tab_1 = [1-2]
+tab_2 = [2-2]
+tab_3 = [3-2]
+tab_4 = [4-2]
+tab_5 = [5-2]
+tab_6 = [6-2]
+tab_7 = [7-2]
+tab_8 = [8-2]
+tab_9 = [9-2]
+tab_10 = [10-2]
+
+for y in tab_1:
+    print("1-2 = {}".format(y))
+for i in tab_2:
+    print("2-2 = {}".format(i))
+for a in tab_3:
+    print("3-2 = {}".format(a))
+for b in tab_4:
+    print("4-2 = {}".format(b))
+for c in tab_5:
+    print("5-2 = {}".format(c))  
+for d in tab_6:
+    print("6-2 = {}".format(d))
+for e in tab_7:
+    print("7-2 = {}".format(e))
+for f in tab_8:
+    print("8-2 = {}".format(f))
+for g in tab_9:
+    print("9-2 = {}".format(g))
+for h in tab_10:
+    print("10-2 = {}".format(h))
+    print ("----------------------------------------------")
+    
+tab_1 = [1-3]
+tab_2 = [2-3]
+tab_3 = [3-3]
+tab_4 = [4-3]
+tab_5 = [5-3]
+tab_6 = [6-3]
+tab_7 = [7-3]
+tab_8 = [8-3]
+tab_9 = [9-3]
+tab_10 = [10-3]
+
+for y in tab_1:
+    print("1-3 = {}".format(y))
+for i in tab_2:
+    print("2-3 = {}".format(i))
+for a in tab_3:
+    print("3-3 = {}".format(a))
+for b in tab_4:
+    print("4-3 = {}".format(b))
+for c in tab_5:
+    print("5-3 = {}".format(c))  
+for d in tab_6:
+    print("6-3 = {}".format(d))
+for e in tab_7:
+    print("7-3 = {}".format(e))
+for f in tab_8:
+    print("8-3 = {}".format(f))
+for g in tab_9:
+    print("9-3 = {}".format(g))
+for h in tab_10:
+    print("10-3 = {}".format(h))
+    print ("----------------------------------------------")
+   
+tab_1 = [1-4]
+tab_2 = [2-4]
+tab_3 = [3-4]
+tab_4 = [4-4]
+tab_5 = [5-4]
+tab_6 = [6-4]
+tab_7 = [7-4]
+tab_8 = [8-4]
+tab_9 = [9-4]
+tab_10 = [10-4]
+
+for y in tab_1:
+    print("1-4 = {}".format(y))
+for i in tab_2:
+    print("2-4 = {}".format(i))
+for a in tab_3:
+    print("3-4 = {}".format(a))
+for b in tab_4:
+    print("4-4 = {}".format(b))
+for c in tab_5:
+    print("5-4 = {}".format(c))  
+for d in tab_6:
+    print("6-4 = {}".format(d))
+for e in tab_7:
+    print("7-4 = {}".format(e))
+for f in tab_8:
+    print("8-4 = {}".format(f))
+for g in tab_9:
+    print("9-4 = {}".format(g))
+for h in tab_10:
+    print("10-4 = {}".format(h))
+    print ("---------------------- Fim ------------------------")
