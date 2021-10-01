34dd9a432dc981241f9be93f97c7759e28a830b3
Mariaalice1 <85592927+Mariaalice1@users.noreply.github.com>
2021-10-01 12:50:37 -0300

    tab.multiplicação

--- tab.py
+++ tab.py
@@ -264,4 +264,136 @@ for h in tab_10:
     print("10/4 = {:.2f}".format(h))
     print ("----------------------------------------------")
 
+#Multiplicacao
+tabb = [1*1]
+tab = [1*2]
+tab_2 = [1*3]
+tab_3 = [1*4]
+tab_4 = [1*5]
+tab_5 = [1*6]
+tab_6 = [1*7]
+tab_7 = [1*8]
+tab_8 = [1*9]
+tab_9 = [1*10]
+
+for y in tabb:
+    print("1x1 = {}".format(y))
+for i in tab:
+    print("1x2 = {}".format(i))
+for a in tab_2:
+    print("1x3 = {}".format(a))
+for b in tab_3:
+    print("1x4 = {}".format(b))
+for c in tab_4:
+    print("1x5 = {}".format(c))  
+for d in tab_5:
+    print("1x6 = {}".format(d))
+for e in tab_6:
+    print("1x7 = {}".format(e))
+for f in tab_7:
+    print("1x8 = {}".format(f))
+for g in tab_8:
+    print("1x9 = {}".format(g))
+for h in tab_9:
+    print("1x10 = {}".format(h))
+    print ("----------------------------------------------")
+    
+tab_1 = [2*1]
+tab_2 = [2*2]
+tab_3 = [2*3]
+tab_4 = [2*4]
+tab_5 = [2*5]
+tab_6 = [2*6]
+tab_7 = [2*7]
+tab_8 = [2*8]
+tab_9 = [2*9]
+tab_10 = [2*10]
+
+for y in tab_1:
+    print("2x1 = {}".format(y))
+for i in tab_2:
+    print("2x2 = {}".format(i))
+for a in tab_3:
+    print("2x3 = {}".format(a))
+for b in tab_4:
+    print("2x4 = {}".format(b))
+for c in tab_5:
+    print("2x5 = {}".format(c))  
+for d in tab_6:
+    print("2x6 = {}".format(d))
+for e in tab_7:
+    print("2x7 = {}".format(e))
+for f in tab_8:
+    print("2x8 = {}".format(f))
+for g in tab_9:
+    print("2x9 = {}".format(g))
+for h in tab_10:
+    print("2x10 = {}".format(h))
+    print ("----------------------------------------------")
+    
+tab_1 = [3*1]
+tab_2 = [3*2]
+tab_3 = [3*3]
+tab_4 = [3*4]
+tab_5 = [3*5]
+tab_6 = [3*6]
+tab_7 = [3*7]
+tab_8 = [3*8]
+tab_9 = [3*9]
+tab_10 = [3*10]
+
+for y in tab_1:
+    print("3x1 = {}".format(y))
+for i in tab_2:
+    print("3x2 = {}".format(i))
+for a in tab_3:
+    print("3x3 = {}".format(a))
+for b in tab_4:
+    print("3x4 = {}".format(b))
+for c in tab_5:
+    print("3x5 = {}".format(c))  
+for d in tab_6:
+    print("3x6 = {}".format(d))
+for e in tab_7:
+    print("3x7 = {}".format(e))
+for f in tab_8:
+    print("3x8 = {}".format(f))
+for g in tab_9:
+    print("3x9 = {}".format(g))
+for h in tab_10:
+    print("3x10 = {}".format(h))
+    print ("----------------------------------------------")
+   
+tab_1 = [4*1]
+tab_2 = [4*2]
+tab_3 = [4*3]
+tab_4 = [4*4]
+tab_5 = [4*5]
+tab_6 = [4*6]
+tab_7 = [4*7]
+tab_8 = [*8]
+tab_9 = [4*9]
+tab_10 = [4*10]
+
+for y in tab_1:
+    print("4x1 = {}".format(y))
+for i in tab_2:
+    print("4x2 = {}".format(i))
+for a in tab_3:
+    print("4x3 = {}".format(a))
+for b in tab_4:
+    print("4x4 = {}".format(b))
+for c in tab_5:
+    print("4x5 = {}".format(c))  
+for d in tab_6:
+    print("4x6 = {}".format(d))
+for e in tab_7:
+    print("4x7 = {}".format(e))
+for f in tab_8:
+    print("4x8 = {}".format(f))
+for g in tab_9:
+    print("4x9 = {}".format(g))
+for h in tab_10:
+    print("4x10 = {}".format(h))
+    print ("----------------------------------------------")
     
