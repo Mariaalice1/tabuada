b0bee603e24dc02972aa1477a7919d09760effa1
Mariaalice1 <85592927+Mariaalice1@users.noreply.github.com>
2021-10-01 12:34:53 -0300

    tab.divisão

--- tab.py
+++ tab.py
@@ -131,4 +131,137 @@ for h in tab_10:
     print("10**4 = {}".format(h))
     print ("----------------------------------------------")
 
+# Divisao
+tabb = [1/1]
+tab = [2/1]
+tab_2 = [3/1]
+tab_3 = [4/1]
+tab_4 = [5/1]
+tab_5 = [6/1]
+tab_6 = [7/1]
+tab_7 = [8/1]
+tab_8 = [9/1]
+tab_9 = [10/1]
+
+for y in tabb:
+    print("1/1 = {}".format(y))
+for i in tab:
+    print("2/1 = {:.2f}".format(i))
+for a in tab_2:
+    print("3/1 = {:.2f}".format(a))
+for b in tab_3:
+    print("4/1 = {:.2f}".format(b))
+for c in tab_4:
+    print("5/1 = {:.2f}".format(c))  
+for d in tab_5:
+    print("6/1 = {:.2f}".format(d))
+for e in tab_6:
+    print("7/1 = {:.2f}".format(e))
+for f in tab_7:
+    print("8/1 = {:.2f}".format(f))
+for g in tab_8:
+    print("9/1 = {:.2f}".format(g))
+for h in tab_9:
+    print("10/1 = {:.2f}".format(h))
+    print ("----------------------------------------------")
+    
+tab_1 = [1/2]
+tab_2 = [2/2]
+tab_3 = [3/2]
+tab_4 = [4/2]
+tab_5 = [5/2]
+tab_6 = [6/2]
+tab_7 = [7/2]
+tab_8 = [8/2]
+tab_9 = [9/2]
+tab_10 = [10/2]
+
+for y in tab_1:
+    print("1/2 = {:.2f}".format(y))
+for i in tab_2:
+    print("2/2 = {:.2f}".format(i))
+for a in tab_3:
+    print("3/2 = {:.2f}".format(a))
+for b in tab_4:
+    print("4/2 = {:.2f}".format(b))
+for c in tab_5:
+    print("5/2 = {:.2f}".format(c))  
+for d in tab_6:
+    print("6/2 = {:.2f}".format(d))
+for e in tab_7:
+    print("7/2 = {:.2f}".format(e))
+for f in tab_8:
+    print("8/2 = {:.2f}".format(f))
+for g in tab_9:
+    print("9/2 = {:.2f}".format(g))
+for h in tab_10:
+    print("10/2 = {:.2f}".format(h))
+    print ("----------------------------------------------")
+    
+tab_1 = [1/3]
+tab_2 = [2/3]
+tab_3 = [3/3]
+tab_4 = [4/3]
+tab_5 = [5/3]
+tab_6 = [6/3]
+tab_7 = [7/3]
+tab_8 = [8/3]
+tab_9 = [9/3]
+tab_10 = [10/3]
+
+for y in tab_1:
+    print("1/3 = {:.2f}".format(y))
+for i in tab_2:
+    print("2/3 = {:.2f}".format(i))
+for a in tab_3:
+    print("3/3 = {:.2f}".format(a))
+for b in tab_4:
+    print("4/3 = {:.2f}".format(b))
+for c in tab_5:
+    print("5/3 = {:.2f}".format(c))  
+for d in tab_6:
+    print("6/3 = {:.2f}".format(d))
+for e in tab_7:
+    print("7/3 = {:.2f}".format(e))
+for f in tab_8:
+    print("8/3 = {:.2f}".format(f))
+for g in tab_9:
+    print("9/3 = {:.2f}".format(g))
+for h in tab_10:
+    print("10/3 = {:.2f}".format(h))
+    print ("----------------------------------------------")
+   
+tab_1 = [1/4]
+tab_2 = [2/4]
+tab_3 = [3/4]
+tab_4 = [4/4]
+tab_5 = [5/4]
+tab_6 = [6/4]
+tab_7 = [7/4]
+tab_8 = [8/4]
+tab_9 = [9/4]
+tab_10 = [10/4]
+
+for y in tab_1:
+    print("1/4 = {:.2f}".format(y))
+for i in tab_2:
+    print("2/4 = {:.2f}".format(i))
+for a in tab_3:
+    print("3/4 = {:.2f}".format(a))
+for b in tab_4:
+    print("4/4 = {:.2f}".format(b))
+for c in tab_5:
+    print("5/4 = {:.2f}".format(c))  
+for d in tab_6:
+    print("6/4 = {:.2f}".format(d))
+for e in tab_7:
+    print("7/4 = {:.2f}".format(e))
+for f in tab_8:
+    print("8/4 = {:.2f}".format(f))
+for g in tab_9:
+    print("9/4 = {:.2f}".format(g))
+for h in tab_10:
+    print("10/4 = {:.2f}".format(h))
+    print ("----------------------------------------------")
+
     
